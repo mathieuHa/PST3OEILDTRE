cecdf78d3f8b6f752aa5c384db7e3abf344f2c8c
mathieuHa <mathieu.hanotaux@gmail.com>
2017-02-14 21:30:14 +0100

    data fixtures add Daily data

--- src/DTRE/OeilBundle/DataFixtures/ORM/LoadDataData.php
+++ src/DTRE/OeilBundle/DataFixtures/ORM/LoadDataData.php
@@ -12,6 +12,7 @@ use DateInterval;
 use DateTimeZone;
 use Doctrine\Common\DataFixtures\FixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
+use DTRE\OeilBundle\Entity\DailyData;
 use DTRE\OeilBundle\Entity\Data;
 use DTRE\OeilBundle\Entity\Sensor;
 
@@ -35,6 +36,11 @@ class LoadDataData implements FixtureInterface
         $data2 = [$max];
         $data3 = [$max];
         $data4 = [$max];
+        $dailydata1 = [$max/48];
+        $dailydata2 = [$max/48];
+        $dailydata3 = [$max/48];
+        $dailydata4 = [$max/48];
+
         $d = null;
         for ($i=0; $i<$max; $i++){
             $d = clone $date;
@@ -58,6 +64,44 @@ class LoadDataData implements FixtureInterface
             $data3[$i]->setValue(rand(-5,25));
             $date->add($interval);
             $s3->addDatum($data3[$i]);
+        }
+        $date = new \DateTime('2017-01-01 00:00:00', new DateTimeZone('Europe/Paris'));
+        $interval = new DateInterval('P0Y0M1DT0H0M0S'); //1min
+        $d = null;
+        for ($i=0; $i<$max/48; $i++){
+            $d = clone $date;
+            $dailydata1[$i] = new DailyData();
+            $dailydata1[$i]->setDate($d);
+            $dailydata1[$i]->setValue(rand(-5,25));
+            $dailydata1[$i]->setMin(rand(-5,15));
+            $dailydata1[$i]->setMax(rand(5,25));
+            $dailydata1[$i]->setNumber(48);
+            $s1->addDailydatum($dailydata1[$i]);
+            $d = clone $date;
+            $dailydata2[$i] = new DailyData();
+            $dailydata2[$i]->setDate($d);
+            $dailydata2[$i]->setValue(rand(-5,25));
+            $dailydata2[$i]->setMin(rand(-5,15));
+            $dailydata2[$i]->setMax(rand(5,25));
+            $dailydata2[$i]->setNumber(48);
+            $s2->addDailydatum($dailydata2[$i]);
+            $d = clone $date;
+            $dailydata3[$i] = new DailyData();
+            $dailydata3[$i]->setDate($d);
+            $dailydata3[$i]->setValue(rand(-5,25));
+            $dailydata3[$i]->setMin(rand(-5,15));
+            $dailydata3[$i]->setMax(rand(5,25));
+            $dailydata3[$i]->setNumber(48);
+            $s3->addDailydatum($dailydata3[$i]);
+            $d = clone $date;
+            $dailydata4[$i] = new DailyData();
+            $dailydata4[$i]->setDate($d);
+            $dailydata4[$i]->setValue(rand(-5,25));
+            $dailydata4[$i]->setMin(rand(-5,15));
+            $dailydata4[$i]->setMax(rand(5,25));
+            $dailydata4[$i]->setNumber(48);
+            $s4->addDailydatum($dailydata4[$i]);
+            $date->add($interval);
         }
 
         $manager->persist($s1);

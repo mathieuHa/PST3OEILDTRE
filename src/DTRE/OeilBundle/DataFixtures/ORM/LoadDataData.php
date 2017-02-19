5c16e05bef871a6faefa5b7fc3b0182f2da4cbdb
mathieuHa <mathieu.hanotaux@gmail.com>
2017-02-19 11:23:52 +0100

    add better rand for day data

--- src/DTRE/OeilBundle/DataFixtures/ORM/LoadDataData.php
+++ src/DTRE/OeilBundle/DataFixtures/ORM/LoadDataData.php
@@ -49,27 +49,48 @@ class LoadDataData implements FixtureInterface
         $dailydata3 = [$max/48];
         $dailydata4 = [$max/48];
 
+        $number1 = rand(-5,25);
+        $oldnumber1 = $number1;
+        $number2 = rand(-5,25);
+        $oldnumber2 = $number2;
+        $number3 = rand(-5,25);
+        $oldnumber3 = $number3;
+        $number4 = rand(-5,25);
+        $oldnumber4 = $number4;
+
         $d = null;
         for ($i=0; $i<$max; $i++){
             $d = clone $date;
             $data1[$i] = new Data();
             $data1[$i]->setDate($d);
-            $data1[$i]->setValue(rand(-5,25));
+            $number1 = rand($oldnumber1-1,$oldnumber1+1);
+            if ($number1<-5) $number1 = -4; if ($number1>25) $number1 = 24;
+            $oldnumber1 = $number1;
+            $data1[$i]->setValue($number1);
             $s1->addDatum($data1[$i]);
             $d = clone $date;
             $data2[$i] = new Data();
             $data2[$i]->setDate($d);
-            $data2[$i]->setValue(rand(-5,25));
+            $number2 = rand($oldnumber2-1,$oldnumber2+1);
+            if ($number2<-5) $number2 = -4; if ($number2>25) $number2 = 24;
+            $oldnumber2 = $number2;
+            $data2[$i]->setValue($number2);
             $s2->addDatum($data2[$i]);
             $d = clone $date;
             $data4[$i] = new Data();
             $data4[$i]->setDate($d);
-            $data4[$i]->setValue(rand(-5,25));
+            $number3 = rand($oldnumber3-1,$oldnumber3+1);
+            if ($number3<-5) $number3 = -4; if ($number3>25) $number3 = 24;
+            $oldnumber3 = $number3;
+            $data4[$i]->setValue($number3);
             $s4->addDatum($data4[$i]);
             $d = clone $date;
             $data3[$i] = new Data();
             $data3[$i]->setDate($d);
-            $data3[$i]->setValue(rand(-5,25));
+            $number4 = rand($oldnumber4-1,$oldnumber4+1);
+            if ($number4<-5) $number4 = -4; if ($number4>25) $number4 = 24;
+            $oldnumber4 = $number4;
+            $data3[$i]->setValue($number4);
             $date->add($interval);
             $s3->addDatum($data3[$i]);
         }
